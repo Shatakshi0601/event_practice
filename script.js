2ce4a36089aae25fc471f27e2cb91c630016b67f
Shatakshi Singh <shatakshi1024@gmail.com>
2024-12-20 05:09:12 +0000

    Update script.js with new content

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function changeColor(color, headingText) {
     document.body.style.backgroundColor = color;
     document.getElementById('heading').textContent = headingText; 
   }
+
+document.getElementById('btn1').addEventListener("click", ()=> {
+  changeColor('red', 'Hello');
+})
+
+document.getElementById('btn2').addEventListener("click", ()=> {
+  changeColor('blue', 'How are you?');
+})
+
+document.getElementById('btn3').addEventListener("click", ()=> {
+  changeColor('green', 'Have a Nice Day');
+})
